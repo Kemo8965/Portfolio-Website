2de3f9ec4813e071816e7989bcb9a30534dd3278
Kemo8965 <78011284+Kemo8965@users.noreply.github.com>
2021-07-11 20:11:13 +0200

    portfolio

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Hero from './pages/Hero'
+function App() {
+  return (
+    <div className="App">
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
